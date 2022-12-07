864fc5b1d211ca98756d06c6df80221c4182c990
Rodolfo Puig <rodolfo@puig.io>
2022-12-07 10:19:39 -0500

    Updating python project configuration

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+import os
+
 from setuptools import setup
 
+
 if __name__ == "__main__":
+    BASEDIR = os.path.dirname(__file__)
+    BASEDIR and os.chdir(BASEDIR)
     setup()
